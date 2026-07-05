ca8f24040350955d761f79ae8e813d592d3ea065
sivaramachakradhar <sivaramisetti700@gmail.com>
2026-07-05 18:54:36 +0530

    updated admincontroller

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -4,13 +4,9 @@ const Order = require("../models/Order");
 
 const getDashboard = async (req, res) => {
     try {
-
         const totalUsers = await User.countDocuments();
-
         const totalBooks = await Book.countDocuments();
-
         const totalOrders = await Order.countDocuments();
-
         const revenue = await Order.aggregate([
             {
                 $group: {
@@ -37,51 +33,37 @@ const getDashboard = async (req, res) => {
 };
 
 const getAllUsers = async (req, res) => {
-
     try {
-
         const users = await User.find()
             .select("-password");
 
         res.status(200).json({
             users
         });
-
     } catch (error) {
-
         res.status(500).json({
             message: error.message
         });
-
     }
-
 };
 
 const getAllBooks = async (req, res) => {
-
     try {
-
         const books = await Book.find()
             .populate("sellerId", "name email");
 
         res.status(200).json({
             books
         });
-
     } catch (error) {
-
         res.status(500).json({
             message: error.message
         });
-
     }
-
 };
 
 const getAllOrders = async (req, res) => {
-
     try {
-
         const orders = await Order.find()
             .populate("userId", "name email")
             .populate("items.bookId", "title");
@@ -89,71 +71,49 @@ const getAllOrders = async (req, res) => {
         res.status(200).json({
             orders
         });
-
     } catch (error) {
-
         res.status(500).json({
             message: error.message
         });
-
     }
-
 };
 
 const deleteUser = async (req, res) => {
-
     try {
-
         const user = await User.findById(req.params.id);
-
         if (!user) {
             return res.status(404).json({
                 message: "User not found"
             });
         }
-
         await user.deleteOne();
-
         res.status(200).json({
             message: "User deleted successfully"
         });
-
     } catch (error) {
-
         res.status(500).json({
             message: error.message
         });
-
     }
-
 };
 
 const deleteBook = async (req, res) => {
-
     try {
-
         const book = await Book.findById(req.params.id);
-
         if (!book) {
             return res.status(404).json({
                 message: "Book not found"
             });
         }
-
         await book.deleteOne();
-
         res.status(200).json({
             message: "Book deleted successfully"
         });
-
     } catch (error) {
-
         res.status(500).json({
             message: error.message
         });
-
     }
-
 };
 
 module.exports = {
